3c3bf7fe7e919b717daf9b7adc3417d9d8a0c3f2
Drian <cfangeles0430@gmail.com>
2025-07-12 09:35:39 +0800

    add client and server folder

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import NotFound from './pages/NotFound';
+import Products from './pages/Product';
+import ProductDetailPage from './pages/ProductDetails';
+
+const App = () => {
+  return (
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/product/:id" element={<ProductDetailPage />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+
+  );
+}
+
+export default App
