3c2a4835b9593fc532b6a8f4039ef17d8251b117
Drian <edrianangeles85@gmail.com>
2025-10-11 13:17:46 +0800

    another typo

--- client/src/components/Home/HeaderRight.tsx
+++ client/src/components/Home/HeaderRight.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import furnitureLogo from '../../assets/AR-Furniture_Logo.png';
-import shopName from '../../assets/Name.png';
+import shopName from '../../assets/NAME.png';
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
